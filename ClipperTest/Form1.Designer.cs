172afe81f44a1398a49ed0e9e85106f70412b91d
Petr Rogov <peterrogov@gmail.com>
2018-07-09 10:51:05 +0200

    Init

--- ClipperTest/Form1.Designer.cs
+++ ClipperTest/Form1.Designer.cs
@@ -28,12 +28,165 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.pbSource = new System.Windows.Forms.PictureBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.pbSolution = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSource)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSolution)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.button3);
+            this.splitContainer1.Panel1.Controls.Add(this.button2);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.tabControl1);
+            this.splitContainer1.Size = new System.Drawing.Size(1197, 584);
+            this.splitContainer1.SplitterDistance = 247;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 287);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(167, 43);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Clip It";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 89);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(167, 40);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Delete Polygon";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 40);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Draw Polygon";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(946, 584);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.pbSource);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(938, 551);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Source";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // pbSource
+            // 
+            this.pbSource.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbSource.Location = new System.Drawing.Point(3, 3);
+            this.pbSource.Name = "pbSource";
+            this.pbSource.Size = new System.Drawing.Size(932, 545);
+            this.pbSource.TabIndex = 0;
+            this.pbSource.TabStop = false;
+            this.pbSource.SizeChanged += new System.EventHandler(this.pbSource_SizeChanged);
+            this.pbSource.Click += new System.EventHandler(this.pbSource_Click);
+            this.pbSource.Paint += new System.Windows.Forms.PaintEventHandler(this.pbSource_Paint);
+            this.pbSource.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pbSource_MouseDown);
+            this.pbSource.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pbSource_MouseMove);
+            this.pbSource.Resize += new System.EventHandler(this.pbSource_Resize);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.pbSolution);
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(938, 551);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Clipped";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // pbSolution
+            // 
+            this.pbSolution.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbSolution.Location = new System.Drawing.Point(3, 3);
+            this.pbSolution.Name = "pbSolution";
+            this.pbSolution.Size = new System.Drawing.Size(932, 545);
+            this.pbSolution.TabIndex = 0;
+            this.pbSolution.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1197, 584);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbSource)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbSolution)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.PictureBox pbSource;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pbSolution;
     }
 }
 
--- ClipperTest/Form1.cs
+++ ClipperTest/Form1.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using ClipperLib;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +14,210 @@ namespace ClipperTest
 {
     public partial class Form1 : Form
     {
+        private List<List<IntPoint>> allPolygons = new List<List<IntPoint>>();
+        private List<List<IntPoint>> clippedPolygons = new List<List<IntPoint>>();
+
+        private List<IntPoint> newPolygon = null;
+        private bool tempPoint = false;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void pbSource_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pbSource_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (null == newPolygon)
+                return;
+
+            newPolygon.Add(new IntPoint(e.X, e.Y));
+            tempPoint = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (null == newPolygon)
+                newPolygon = new List<IntPoint>();
+            else
+            {
+                if (newPolygon.Count < 3)
+                    return;
+
+                if (tempPoint)
+                    newPolygon.RemoveAt(newPolygon.Count - 1);
+
+                allPolygons.Add(newPolygon);
+                newPolygon = null;
+                RedrawSource();
+            }
+        }
+
+        private void RedrawSolution()
+        {
+            var formGraphics = pbSolution.CreateGraphics();
+            formGraphics.Clear(Color.White);
+            foreach (var poly in clippedPolygons)
+                DrawPolygon(pbSolution, poly);
+        }
+
+        private void RedrawSource()
+        {
+            var formGraphics = pbSource.CreateGraphics();
+            formGraphics.Clear(Color.White);
+            foreach (var poly in allPolygons)
+                DrawPolygon(pbSource, poly);
+
+            DrawPolygon(pbSource, newPolygon, false);
+        }
+
+        private void DrawPolygon(PictureBox pb, List<IntPoint> polygon, bool filled = true)
+        {
+            if (null == polygon || polygon.Count < 3)
+                return;
+
+            var formGraphics = pb.CreateGraphics();
+        
+            Point[] points = new Point[polygon.Count];
+            for (var i = 0; i < polygon.Count; i++)
+            {
+                points[i] = new Point((int)polygon[i].X, (int)polygon[i].Y);
+            }
+
+            var myBrush = new SolidBrush(Color.FromArgb(100, 255, 0, 0));
+            var myPen = new Pen(Color.FromArgb(255, 0, 0, 255));
+            if (filled)
+            {
+                formGraphics.FillPolygon(myBrush, points);
+                formGraphics.DrawPolygon(myPen, points);
+            }
+            else
+            {
+                formGraphics.DrawPolygon(myPen, points);
+            }
+            myBrush.Dispose();
+            myPen.Dispose();
+            formGraphics.Dispose();
+        }
+
+        private void pbSource_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (null == newPolygon || newPolygon.Count == 0)
+                return;
+
+            Point end = new Point(e.X, e.Y);
+
+            if (tempPoint)
+                newPolygon.RemoveAt(newPolygon.Count - 1);
+
+            newPolygon.Add(new IntPoint(end.X, end.Y));
+            tempPoint = true;
+
+            RedrawSource();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+        }
+
+        private void pbSource_Resize(object sender, EventArgs e)
+        {
+        }
+
+        private void pbSource_SizeChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+            RedrawSource();
+        }
+
+        private void pbSource_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ClipPolygons();
+        }
+
+        private void ClipPolygons()
+        {
+            clippedPolygons = new List<List<IntPoint>>();
+            clippedPolygons.AddRange(allPolygons);
+            bool collectionModified = true;
+            while (collectionModified)
+            {
+                collectionModified = false;
+                for (var oPoly = 0; oPoly < clippedPolygons.Count; oPoly++)
+                {
+                    var poly1 = clippedPolygons[oPoly];
+
+                    for (var iPoly = 0; iPoly < clippedPolygons.Count; iPoly++)
+                    {
+                        if (oPoly == iPoly)
+                            continue;
+
+                        var poly2 = clippedPolygons[iPoly];
+
+                        Clipper clip = new Clipper();
+                        clip.AddPath(poly1, PolyType.ptSubject, true);
+                        clip.AddPath(poly2, PolyType.ptClip, true);
+
+                        List<List<IntPoint>> solution = new List<List<IntPoint>>();
+
+                        clip.Execute(ClipType.ctIntersection, solution);
+
+                        if (solution.Count == 0)
+                            continue;
+
+                        foreach (var intersection in solution)
+                        {
+                            clippedPolygons.Add(intersection);
+                        }
+
+                        solution = new List<List<IntPoint>>();
+                        clip.Clear();
+                        clip.AddPath(poly1, PolyType.ptSubject, true);
+                        clip.AddPath(poly2, PolyType.ptClip, true);
+
+                        clip.Execute(ClipType.ctDifference, solution);
+
+                        foreach (var difference in solution)
+                        {
+                            clippedPolygons.Add(difference);
+                        }
+
+                        solution = new List<List<IntPoint>>();
+                        clip.Clear();
+                        clip.AddPath(poly1, PolyType.ptClip, true);
+                        clip.AddPath(poly2, PolyType.ptSubject, true);
+
+                        clip.Execute(ClipType.ctDifference, solution);
+
+                        foreach (var difference in solution)
+                        {
+                            clippedPolygons.Add(difference);
+                        }
+
+                        clippedPolygons.Remove(poly1);
+                        clippedPolygons.Remove(poly2);
+                        collectionModified = true;
+                        RedrawSolution();
+                        Thread.Sleep(500);
+                        break;
+                    }
+
+                    if (collectionModified)
+                        break;
+                }
+            }
         }
     }
 }
